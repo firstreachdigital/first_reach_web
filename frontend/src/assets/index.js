242e147e32bd7ba712fd01e09c398b6016733be5
shabeeb1044 <126136737+shabeeb1044@users.noreply.github.com>
2026-07-31 16:49:23 +0530

    services image changed

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -3,14 +3,14 @@ export { default as image_2 } from "./image_2.png";
 export { default as image_3 } from "./image_3.png";
 export { default as image_4 } from "./image_4.png";
 
-export { default as Brand } from "./Brand.png";
-export { default as Brandpro } from "./Brandpro.jpeg";
-export { default as copywrite } from "./copywrite.jpeg";
-export { default as influencer } from "./influencer.png";
-export { default as media } from "./media.jpeg";
-export { default as Seo } from "./Seo.png";
-export { default as SocialMedia } from "./SocialMedia.jpeg";
-export { default as web } from "./web.png";
+export { default as Brand } from "./Brand Development.jpeg";
+export { default as Brandpro } from "./Brand Protection.jpeg";
+export { default as copywrite } from "./Copywriting.jpeg";
+export { default as influencer } from "./InfluencerMarketing.jpeg";
+export { default as media } from "./Media Productions.jpeg";
+export { default as Seo } from "./SEO.jpeg";
+export { default as SocialMedia } from "./Social Media Management.jpeg";
+export { default as web } from "./Website and App.jpeg";
 
 export { default as Collaboration } from "./Collaboration.png";
 export { default as Communication } from "./Communication.png";
